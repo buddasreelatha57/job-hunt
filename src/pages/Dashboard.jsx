95ef278907ae9f6ad02a787338053464c843a336
sree <149601878+buddasreelatha57@users.noreply.github.com>
2026-06-03 00:08:26 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ export default function Dashboard() {
       setLoading(true);
 
       const res = await axios.get(
-        "http://127.0.0.1:5000/api/jobs"
+        "https://job-hunt-kpht.onrender.com/api/jobs"
       );
 
       // GET LOCAL STORAGE DATA
