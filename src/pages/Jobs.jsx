91e416fbdc2bb8383a88206fa67ee45995569b42
sree <149601878+buddasreelatha57@users.noreply.github.com>
2026-06-03 00:05:46 +0530

    Update Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -49,7 +49,7 @@ export default function Jobs() {
     try {
 
       const res = await axios.get(
-        "http://127.0.0.1:5000/api/jobs"
+        "https://job-hunt-kpht.onrender.com/api/jobs"
       );
 
       const savedJobs =
